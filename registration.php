954c17f06c56e6330c426e323f644dca49c0997d
islammusicscv <islam.music@scv.si>
2024-02-05 16:37:06 +0100

    Urejen dizajn registracije

--- registration.php
+++ registration.php
@@ -2,15 +2,29 @@
     include_once 'header.php';
 ?>
 
-<h1>Registracija</h1>
-
 <form action="user_insert.php" method="post">
-    <input type="text" name="first_name" placeholder="Vnesi svoje ime" required="required" /><br />
-    <input type="text" name="last_name" placeholder="Vnesi svoj priimek" required="required" /><br />
-    <input type="text" name="telephone" placeholder="Vnesi svoj telefon" /><br />
-    <input type="email" name="email" placeholder="Vnesi svojo e-pošto" required="required" /><br />
-    <input type="password" name="pass" placeholder="Vnesi svoje geslo" required="required" /><br />
-    <input type="submit" value="Registriraj" />
+    <h1 class="h3 mb-3 fw-normal">Registracija</h1>
+    <div class="form-floating">
+        <input type="text" id="floatingInput1" class="form-control" name="first_name" placeholder="Vnesi svoje ime" required="required" /><br />
+        <label for="floatingInput1">Vnesi ime</label>
+    </div>
+    <div class="form-floating">
+        <input type="text" id="floatingInput2" class="form-control" name="last_name" placeholder="Vnesi svoj priimek" required="required" /><br />
+        <label for="floatingInput2">Vnesi priimek</label>
+    </div>
+    <div class="form-floating">
+        <input type="text" id="floatingInput3" class="form-control" name="telephone" placeholder="Vnesi svoj telefon" /><br />
+        <label for="floatingInput3">Vnesi telefon</label>
+    </div>
+    <div class="form-floating">
+        <input type="email" id="floatingInput4" class="form-control" name="email" placeholder="Vnesi svojo e-pošto" required="required" /><br />
+        <label for="floatingInput4">Vnesi e-pošto</label>
+    </div>
+    <div class="form-floating">
+        <input type="password" id="floatingInput5" class="form-control" name="pass" placeholder="Vnesi svoje geslo" required="required" /><br />
+        <label for="floatingInput5">Vnesi geslo</label>
+    </div>
+    <input type="submit" class="btn btn-primary w-100 py-2" value="Registriraj" />
 </form>
 
 <?php
