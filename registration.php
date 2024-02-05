f46747f30106140c36f9c493b43ef59fd1b42111
islammusicscv <islam.music@scv.si>
2024-02-05 16:54:30 +0100

    Registracija - spustni meni - kraji

--- registration.php
+++ registration.php
@@ -13,6 +13,21 @@
         <label for="floatingInput2">Vnesi priimek</label>
     </div>
     <div class="form-floating">
+        <select name="city_id" required="required" id="floatingSelect" class="form-select">
+            <?php
+            include_once 'db.php';
+            $query = "SELECT * FROM cities";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute();
+
+            while($row = $stmt->fetch()) {
+                echo '<option value="'.$row['id'].'">'.$row['title'].'</option>';
+            }
+            ?>
+        </select><br />
+        <label for="floatingSelect">Izberi kraj</label>
+    </div>
+    <div class="form-floating">
         <input type="text" id="floatingInput3" class="form-control" name="telephone" placeholder="Vnesi svoj telefon" /><br />
         <label for="floatingInput3">Vnesi telefon</label>
     </div>
